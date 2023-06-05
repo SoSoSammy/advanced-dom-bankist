4c93294faf6117143392c5a03355cc4888699521
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-05 15:21:08 -0600

    Add section: DOM traversing

--- script.js
+++ script.js
@@ -258,3 +258,35 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log("NAV", e.target, e.currentTarget);
 // });
+
+///////////////////////////////////////
+// DOM Traversing
+
+// const h1 = document.querySelector("h1");
+
+// // Going downwards: child
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// console.log(h1.children); // Works only for direct children
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "orangered";
+
+// // Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest(".header").style.background = "var(--gradient-secondary)"; // .closest is useful for event delegation
+// h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// // Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// // Nodes
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children); // includes h1 element
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = "scale(0.5)";
+// });
